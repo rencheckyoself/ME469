b3cb5bb2afe7a653d57daa01dfd2dacb6c164ed5
= <rencheck.1@live.com>
2019-10-19 21:22:17 -0500

    Astar functional for c.s. of 1, need to debug c.s. 0.1

--- HW1/a_star.py
+++ HW1/a_star.py
@@ -3,7 +3,7 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import heapq
-
+from pprint import pprint
 def load_data(file_name, header, footer, cols):
     """Loads in data from a text file
        Function to import data from a file
@@ -41,7 +41,7 @@ def plot_map(landmark_data):
 
     fig, ax = plt.subplots()
 
-    ax.plot(land_x, land_y, 'ro', markersize=3)
+    ax.plot(land_x, land_y, 'bo', markersize=3)
 
     # cirx = plt.gca()
     #
@@ -54,7 +54,7 @@ class Node(object):
     """
     def __init__(self, next_id, par_id, x_pos, y_pos):
 
-        self.f_val = np.random.rand() #f
+        self.f_val = 0 #f
         self.h_val = 0 #h
         self.g_val = 0 #g
         self.node_id = next_id #node_id
@@ -64,17 +64,19 @@ class Node(object):
         self.x_pos_g = 0 #grid x_pos
         self.y_pos_g = 0 #grid y_pos
 
+        self.sort_val = self.f_val + self.h_val #sort value to optimize heap
+
 
     def __lt__(self, other):
-        return self.f_val < other.f_val
+        return self.sort_val < other.sort_val
 
 class AStar(object):
     """
     contains logic for A* algoritim
     """
-    def __init__(self, start_coord, goal_coord, cell_size):
+    def __init__(self, start_coord, goal_coord, cell_size, inflate):
 
-        self.field = Grid(cell_size)
+        self.field = Grid(cell_size, inflate)
 
         self.odom_data = load_data('ds1_Odometry.dat', 3, 0, [0, 4, 5])
         self.meas_data = load_data('ds1_Measurement.dat', 3, 0, [0, 4, 6, 7])
@@ -91,7 +93,7 @@ class AStar(object):
 
         self.next_id = 2
 
-        self.open_list = [self.start_node, self.goal_node]
+        self.open_list = [self.start_node]
         self.closed_list = []
 
         heapq.heapify(self.open_list)
@@ -99,11 +101,12 @@ class AStar(object):
         result = self.find_path()
 
         # Plot all nodes function
-        self.plot_closed_list()
+        # self.plot_all_nodes()
 
         # Plot final path function
         if result == 1:
             self.plot_final_path()
+
         else:
             print "Did not find the goal. Output code " + str(result)
 
@@ -113,13 +116,13 @@ class AStar(object):
         A* Search Algorithm
         """
 
-
         found_end = 0
-
+        counter = 0
         while found_end == 0:
+            counter += 1
 
             if len(self.open_list) == 0:
-                found_end == 2
+                found_end = 2
 
             eval_node = heapq.heappop(self.open_list)
 
@@ -128,6 +131,10 @@ class AStar(object):
                 found_end = 1
                 self.closed_list.append(eval_node)
             else:
+
+                if counter % 1000 == 0:
+                    print counter
+                    print eval_node.h_val
 
                 self.closed_list.append(eval_node)
                 self.analyze_neighbors(eval_node)
@@ -145,77 +152,82 @@ class AStar(object):
         for _ig, x in enumerate(x_search):
             for _ig, y in enumerate(y_search):
 
-                skip = 0
-
                 pot_node_x_w = cur_node.x_pos_w + x
                 pot_node_y_w = cur_node.y_pos_w + y
-
+                # print x, y, pot_node_x_w, pot_node_y_w
                 # quick skip for 0,0 shift
                 if x == 0 and y == 0:
 
-                    skip = 1
+                    continue
 
                 #check if node is out of bounds, if so skip over
                 elif (pot_node_x_w < self.field.x_axis[0] or
                       pot_node_x_w > self.field.x_axis[1] or
                       pot_node_y_w < self.field.y_axis[0] or
-                      pot_node_y_w < self.field.y_axis[1]):
+                      pot_node_y_w > self.field.y_axis[1]):
 
-                    skip = 1
+                    continue
 
                 else:
                 # check if node is on the closed list, if so skip over
+                    on_cl = 0
                     for _ig, closed_node in enumerate(self.closed_list):
                         if (pot_node_x_w == closed_node.x_pos_w and
                                 pot_node_y_w == closed_node.y_pos_w):
 
-                            skip = 1
+                            on_cl = 1
+                            break
 
-                if skip == 0:
+                if on_cl == 1:
+                    continue
 
-                    match_found = 0
+                match_found = 0
 
-                    # check if neighbor is on the open list
-                    for _ig, open_node in enumerate(self.open_list):
+                # check if neighbor is on the open list
+                for _ig, open_node in enumerate(self.open_list):
 
-                        if (pot_node_x_w == open_node.x_pos_w and
-                                pot_node_y_w == open_node.y_pos_w):
+                    if (pot_node_x_w == open_node.x_pos_w and
+                            pot_node_y_w == open_node.y_pos_w):
 
-                            # if a match is found then compare g values.
-                            match_found = 1
-                            temp_g = self.calc_g(open_node, cur_node, x, y)
+                        # if a match is found then compare g values.
+                        match_found = 1
+                        temp_g = self.calc_g(open_node, cur_node, x, y)
 
-                            if temp_g < open_node.g_val:
-                                open_node.g_val = temp_g
-                                open_node.f_val = temp_g + open_node.h_val
-                                open_node.parent_id = cur_node.node_id
+                        if temp_g < open_node.g_val:
+                            open_node.g_val = temp_g
+                            open_node.f_val = temp_g + open_node.h_val
+                            open_node.parent_id = cur_node.node_id
 
-                            if match_found == 1:
-                                break
+                        if match_found == 1:
+                            break
 
-                    if match_found == 0:
+                if match_found == 0:
 
-                        # create a new node
-                        nei_node = Node(self.next_id, cur_node.node_id, pot_node_x_w, pot_node_y_w)
+                    # create a new node
+                    nei_node = Node(self.next_id, cur_node.node_id, pot_node_x_w, pot_node_y_w)
 
-                        if check_for_goal(nei_node):
-                            nei_node = self.goal_node
+                    if self.check_for_goal(nei_node):
+                        nei_node = self.goal_node
+                        nei_node.parent_id = cur_node.node_id
 
-                        nei_node.x_pos_g, nei_node.y_pos_g = self.field.get_grid_index(nei_node.x_pos_w, nei_node.y_pos_w)
+                    nei_node.x_pos_g, nei_node.y_pos_g = self.field.get_grid_index(nei_node.x_pos_w, nei_node.y_pos_w)
 
-                        nei_node.g_val = self.calc_g(nei_node, cur_node, x, y)
-                        nei_node.h_val = self.calc_h(nei_node)
-                        nei_node.f_val = nei_node.g_val + nei_node.h_val
+                    nei_node.g_val = self.calc_g(nei_node, cur_node, x, y)
+                    nei_node.h_val = self.calc_h(nei_node)
+                    nei_node.f_val = nei_node.g_val + nei_node.h_val
 
-                        self.next_id += 1
-                        heapq.heappush(self.open_list, nei_node)
+                    nei_node.sort_val = nei_node.f_val + nei_node.h_val
+
+                    self.next_id += 1
+
+                    heapq.heappush(self.open_list, nei_node)
 
     def calc_g(self, node_new, node_cur, shift_x, shift_y):
         """
         Calculate true cost, g
         """
 
-        if self.field.cell_cost(node_new.x_pos_g, node_new.y_pos_g) != 1000:
+        if self.field.cell_cost[node_new.x_pos_g, node_new.y_pos_g] != 1000:
             g_new = node_cur.g_val + (shift_x**2 + shift_y**2)
         else:
             g_new = node_cur.g_val + 1000
@@ -234,9 +246,12 @@ class AStar(object):
 
         return h_new
 
-    def check_for_goal(self,node):
-        if (self.goal_node.x_pos_w == node.x_pos_w and
-            self.goal_node.y_pos_w == node.y_pos_w):
+    def check_for_goal(self, node):
+        """
+        check if node is the goal node
+        """
+        if(self.goal_node.x_pos_w == node.x_pos_w and
+           self.goal_node.y_pos_w == node.y_pos_w):
 
             check = True
         else:
@@ -248,7 +263,7 @@ class AStar(object):
         """
         Plot all nodes analyzed
         """
-        x_arr = np.zeros(len(self.closed_list))
+        x_arr = np.zeros(len(self.closed_list) + len(self.open_list))
         y_arr = x_arr[:]
 
         for n_i, node in enumerate(self.closed_list):
@@ -258,6 +273,9 @@ class AStar(object):
         plt.plot(x_arr, y_arr, 'ko', markersize=3)
 
     def plot_final_path(self):
+        """
+        plot the final path
+        """
 
         x_arr = [self.goal_node.x_pos_w]
         y_arr = [self.goal_node.y_pos_w]
@@ -266,18 +284,14 @@ class AStar(object):
 
         while next_par_id is not None:
 
-            match_found = 0
-            # find node
-            while match_found == 0:
+            for _ig, node in enumerate(self.closed_list):
 
-                for _ig, node in enumerate(self.closed_list):
-
-                    if node.node_id == next_par_id:
-                        x_arr.append(node.x_pos_w)
-                        y_arr.append(node.y_pos_w)
-                        next_par_id = node.parent_id
-
-                        break
+                if node.node_id == next_par_id:
+                    # pprint(vars(node))
+                    x_arr.append(node.x_pos_w)
+                    y_arr.append(node.y_pos_w)
+                    next_par_id = node.parent_id
+                    continue
 
         plt.plot(x_arr, y_arr, 'ro-', markersize=4, linewidth=2)
 
@@ -289,7 +303,7 @@ class Grid(object):
     construct map with obstacles
     """
 
-    def __init__(self, width):
+    def __init__(self, width, addition):
 
         self.cell_size = width
 
@@ -306,19 +320,35 @@ class Grid(object):
         # compare landmark location to x and y values
         for _x, marker in enumerate(self.marker_data):
 
-            x_hit, y_hit = get_grid_index(marker[1], marker[2])
+            x_hit_min, y_hit_min = self.get_grid_index(marker[1] - addition, marker[2] - addition)
+
+            x_hit_max, y_hit_max = self.get_grid_index(marker[1] + addition, marker[2] + addition)
+
+            x_hit = np.arange(x_hit_min, x_hit_max, 1)
+            y_hit = np.arange(y_hit_min, y_hit_max, 1)
+
+            if len(x_hit) == 0:
+                x_hit = [x_hit_min]
+            if len(y_hit) == 0:
+                y_hit = [y_hit_min]
 
             #adjust value in cost map to identify obstables
             for _ig, mark_x in enumerate(x_hit):
                 for _ig, mark_y in enumerate(y_hit):
+
                     self.cell_cost[mark_x][mark_y] = 1000
 
         plot_map(self.marker_data)
 
         plt.xticks(np.arange(self.x_axis[0], self.x_axis[1]+self.cell_size, 1))
+        # plt.xticks(np.arange(self.x_axis[0], self.x_axis[1]+self.cell_size, .1))
         plt.yticks(np.arange(self.y_axis[0], self.y_axis[1]+self.cell_size, 1))
+        # plt.yticks(np.arange(self.y_axis[0], self.y_axis[1]+self.cell_size, .1))
+
         plt.imshow(self.cell_cost.T, cmap="Set3", origin='lower', extent=[-2, 5, -6, 6])
-        plt.grid(True, color='k', linewidth=1)
+
+        plt.grid(which="major", color='k', linewidth=1)
+        plt.grid(which="minor", color='-k')
 
         plt.axis([-2, 5, -6, 6])
 
@@ -328,18 +358,18 @@ class Grid(object):
 
         INPUT: data_set_x, data_set_y world position x and y value
 
-        OUTPUT: x, y grid index values that match the given x and y values
+        OUTPUT: x, y grid INDEX values that match the given x and y values
 
         """
 
         for x, graph_x in enumerate(self.x_lim):
             if (data_set_x >= graph_x and
                     data_set_x <= graph_x + self.cell_size):
-                        grid_x = x
+                grid_x = x
 
         for y, graph_y in enumerate(self.y_lim):
             if (data_set_y >= graph_y and
                     data_set_y <= graph_y + self.cell_size):
-                        grid_y  = y
+                grid_y = y
 
         return grid_x, grid_y
--- HW1/run_me.py
+++ HW1/run_me.py
@@ -6,6 +6,14 @@ import a_star
 def main():
     """Main Routine"""
 
-    a_star.AStar([.5, .5], [4.5, 4.5], 1)
+    # pos_set_one = a_star.AStar([.5, -1.5], [0.5, 1.5], 1, 0)
+    # pos_set_two = a_star.AStar([4.5, 3.5], [4.5, -1.5], 1, 0)
+    # pos_set_three = a_star.AStar([-0.5, 5.5], [1.5, -3.5], 1, 0)
+
+    pos_set_four = a_star.AStar([2.45, -3.55], [0.95, -1.55], .1, .3)
+    # pos_set_five = a_star.AStar([4.95, -0.5], [2.45, 0.25], 1)
+    # pos_set_six = a_star.AStar([-0.55, 1.45], [1.95, 3.95], 1)
+
+    pos_set_four.plot_show()
 
 main()
